2b0d7ef2839ba36f2b13253f1d60a10aca4c610d
Piotr Deszyński <deszynski@red-sky.pl>
2022-01-27 21:06:40 +0100

    feat(filtering): allow filtering of offers
    feat(filtering): allow filtering of offers

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,7 @@
 const fetch = require("node-fetch");
+const { URLSearchParams } = require("url");
 
-exports.sourceNodes = async ({ actions }, { companyName }) => {
+exports.sourceNodes = async ({ actions }, { companyName, department, tag }) => {
   const { createNode } = actions;
 
   if (!companyName) {
@@ -9,7 +10,10 @@ exports.sourceNodes = async ({ actions }, { companyName }) => {
     );
     return;
   }
-  const resp = await fetch(`https://${companyName}.recruitee.com/api/offers/`);
+  const resp = await fetch(`https://${companyName}.recruitee.com/api/offers/?${new URLSearchParams({
+    department,
+    tag,
+  })}`);
   const data = await resp.json();
 
   data.offers.forEach(offer =>
