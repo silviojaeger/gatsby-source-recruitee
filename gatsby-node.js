8e20ab587142ba7018043b94d42d5e9e68d0456f
Piotr Deszyński <deszynski@red-sky.pl>
2022-09-20 11:09:37 +0200

    fix(empty-array): fixed plugin to work on an empty array

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,22 @@ const fetch = require("node-fetch");
 const { URLSearchParams } = require("url");
 const camelcaseKeys = require('camelcase-keys');
 
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+
+  createTypes(`
+    type RecruiteeOffer implements Node {
+        id: ID!
+        title: String!
+        employmentTypeCode: String!
+        slug: String!
+        careersUrl: String!
+        careersApplyUrl: String!
+        position: Int!
+    }
+  `)
+}
+
 exports.sourceNodes = async ({ actions }, { companyName, department, tag }) => {
   const { createNode } = actions;
 
@@ -15,11 +31,12 @@ exports.sourceNodes = async ({ actions }, { companyName, department, tag }) => {
     ...(department && { department }),
     ...(tag && { tag }),
   })}`);
+
   const data = await resp.json();
 
   data.offers.forEach(offer =>
     createNode({
-      ...camelcaseKeys(offer, { deep: true }),
+      ...camelcaseKeys(offer),
 
       id: `recruitee-${offer.id}`,
       parent: null,
